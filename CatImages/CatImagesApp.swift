52e8b7a4caa1c1efaf715a5e7fba8fcb69f934d8
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-09-05 17:15:07 +0200

    Initial Commit

---
+++ CatImages/CatImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatImagesApp.swift
+//  CatImages
+//
+//  Created by Yuri Petrosyan on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CatImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
